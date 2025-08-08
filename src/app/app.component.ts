4614effac48e005640fdd4922903f301874c3106
Phuqui238 <qui.np03795@sinhvien.hoasen.edu.vn>
2025-08-08 09:18:57 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './component/header/header.component';
+import {FooterComponent} from './component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterOutlet, HeaderComponent]
+  imports: [RouterOutlet, HeaderComponent, FooterComponent]
 })
 export class AppComponent {}
 
